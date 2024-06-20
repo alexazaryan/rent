8231c7a36929e6ec725e71d6b4db17dcc8216b78
Student <azaryanmail@gmail.com>
2024-06-20 16:13:28 +0300

    assembly of ready-made style

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="header__container">
+      <div className="header__wrap">
+        <h1>header</h1>
+      </div>
+    </div>
+  );
+}
