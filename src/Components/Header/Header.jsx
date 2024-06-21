04f18b74b498efcf68ce8713bfeee8b03b50a551
Student <azaryanmail@gmail.com>
2024-06-21 18:49:57 +0300

    header line

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,8 +2,29 @@ export default function Header() {
   return (
     <div className="header__container">
       <div className="header__wrap">
-        <h1>header</h1>
+        {/* ----------------------------------------------------------------- */}
+        <div className="header__content">
+          <div className="header__logo">
+            <img
+              className="header__logo__img"
+              src="./img/logo.png"
+              alt="logo"
+            />
+          </div>
+
+          <ul className="header__brand__ul">
+            <li className="header__brand">Doremi</li>
+            <li className="header__brand__li">Buy, Sell Or Rent</li>
+          </ul>
+        </div>
+
+        <div className="sandwich__menu">
+          <div className="sandwich__menu__line"></div>
+          <div className="sandwich__menu__line sandwich__menu__line_second"></div>
+          <div className="sandwich__menu__line"></div>
+        </div>
       </div>
+      <div className="header__bg"></div>
     </div>
   );
 }
